ad167065d965efccb0681acd5514924e74955a11
Domderon <dominik.scherer@DUS-WKS-B1472.ubisoft.org>
2014-06-13 10:39:13 +0200

    Updated btc addresses

--- javascripts/icecream.js
+++ javascripts/icecream.js
@@ -2,11 +2,11 @@ var test = angular.module("icecream", ["monospaced.qrcode"]);
 
 function IcecreamCtrl($scope, $http) {
     $scope.items = [
-    { id:'flutschfinger',   name:'Flutsch Finger',       address:'', price:0.001, euro:0.5 },
-    { id:'magnumalmond',    name:'Magnum Almond',        address:'1GCnrpwYfn7AuQVc1XFUyS49Y5LCqzKXSE', price:0.002, euro:1.0 },
-//    { id:'magnumyoghurt',   name:'Magnum Yoghurt Fresh', address:'1K36b7ynjNFttzp79EwXepQq6X8mdYYqse', price:0.002, euro:1.0 },
-    { id:'split',           name:'CujaMara Split',       address:'13CtDQuMqdKu9RuuLKyYTigCQhTLerDMdR', price:0.0016, euro:0.8 },
-//    { id:'snickers',        name:'Snickers Ice Cream',   address:'16iHf9tTvTtX9dchqcnU8VVDj89gc2sHkN', price:0.0016, euro:0.8 },
+    { id:'flutschfinger',   name:'Flutsch Finger',       address:'1NZFMywLV4vZDqtFPEoeF58Ym7vo5oA7eQ', price:0.001, euro:0.5 },
+    { id:'magnumalmond',    name:'Magnum Almond',        address:'13JFwsGPLNtQnkgPYcqugKnkFwaiE1dUTt', price:0.002, euro:1.0 },
+//    { id:'magnumyoghurt',   name:'Magnum Yoghurt Fresh', address:'', price:0.002, euro:1.0 },
+    { id:'split',           name:'CujaMara Split',       address:'16qwAKeC9xqEUfzid85uvSVMqXz5SSFvCW', price:0.0016, euro:0.8 },
+//    { id:'snickers',        name:'Snickers Ice Cream',   address:'', price:0.0016, euro:0.8 },
 //    { id:'magnumgold',      name:'Magnum Gold',          address:'1FyYcrf4QGg9RxnQRkg7xTv7DCirVDYYri', price:0.002, euro:1.0 },
 //    { id:'magnumclassic',   name:'Magnum Classic',       address:'18KBcPgdsk1Q6MMEBkM7wh5TPJ38fYsxpP', price:0.002, euro:1.0 },
 //    { id:'magnummini',      name:'Magnum Mini',          address:'17G2pAaupxEDTSxoVZL3CQrCxBbTD6b6WS', price:0.0016, euro:0.8 },
