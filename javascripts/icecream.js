5f1554e7eb59f2ecc623bd9dc0bc6afba84c7d80
Domderon <dominik@scherer.de>
2014-07-21 11:07:48 +0200

    Updated bitcoin adresses

--- javascripts/icecream.js
+++ javascripts/icecream.js
@@ -9,14 +9,14 @@ test.config([
 
 function IcecreamCtrl($scope, $http) {
     $scope.items = [
-    { id:'flutschfinger',   name:'Flutsch Finger',       address:'1NZFMywLV4vZDqtFPEoeF58Ym7vo5oA7eQ', price:0.001, euro:0.5 },
-    { id:'split',           name:'CujaMara Split',       address:'16qwAKeC9xqEUfzid85uvSVMqXz5SSFvCW', price:0.0016, euro:0.8 },
-    { id:'snickers',        name:'Snickers Ice Cream',   address:'1EnqYmVHS1h5eBeP3SHSLZYmDr2pAZAuiR', price:0.0016, euro:0.8 },
-    { id:'magnummini',      name:'Magnum Mini',          address:'1Ci5tZrC2opG15oW4TWdNT8vEj922nuMqe', price:0.0013, euro:0.7 },
-//    { id:'magnumalmond',    name:'Magnum Almond',        address:'13JFwsGPLNtQnkgPYcqugKnkFwaiE1dUTt', price:0.002, euro:1.0 },
-    { id:'magnumyoghurt',   name:'Magnum Yoghurt Fresh', address:'1JneKd8QbToVo2TNvrgEkQQrAz9MrbGVXm', price:0.002, euro:1.0 },
-   { id:'magnumgold',      name:'Magnum Gold',          address:'1PgPdq7XgSdyV8uhY22Cj77LAxSn1HgrUz', price:0.002, euro:1.0 },
-   { id:'magnumclassic',   name:'Magnum Classic',       address:'1PduF4SebKT7QxKoSzUrwN9D4miSZyF9Yj', price:0.002, euro:1.0 },
+    { id:'magnumclassic',   name:'Magnum Classic',       address:'1PduF4SebKT7QxKoSzUrwN9D4miSZyF9Yj', price:0.002, euro:1.0 },
+    { id:'magnumgold',      name:'Magnum Gold',          address:'1PgPdq7XgSdyV8uhY22Cj77LAxSn1HgrUz', price:0.002, euro:1.0 },
+    { id:'magnumalmond',    name:'Magnum Almond',        address:'1KjeRKq52ipXjcaDq8pMvtvJvWeVuHHW4F', price:0.002, euro:1.0 },
+    { id:'magnumyoghurt',   name:'Magnum Yoghurt Fresh', address:'13uNdtFG5Aat5Vwjfze1dYdnjnarbwKFma', price:0.002, euro:1.0 },
+    { id:'flutschfinger',   name:'Flutsch Finger',       address:'1EeX9JGgicy8nowntp7e88xa7iHKwZhETY', price:0.001, euro:0.5 },
+//    { id:'split',           name:'CujaMara Split',       address:'16qwAKeC9xqEUfzid85uvSVMqXz5SSFvCW', price:0.0016, euro:0.8 },
+    { id:'snickers',        name:'Snickers Ice Cream',   address:'12z2fCDTHQec7UJrpeStVwdPS22T7GXoW5', price:0.0016, euro:0.8 },
+    { id:'magnummini',      name:'Magnum Mini',          address:'12dxXp3wVJmzrzqxGSh3GfEQ3C2ruUcLpf', price:0.0013, euro:0.7 },
 //    { id:'magnummini',      name:'Magnum Mini',          address:'17G2pAaupxEDTSxoVZL3CQrCxBbTD6b6WS', price:0.0016, euro:0.8 },
     ];
 
